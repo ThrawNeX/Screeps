3cf53550c0eabb33d03a99afa9f96e4a39c16367
mmezger <mmezger@stud.hs-heilbronn.de>
2017-07-25 17:40:16 +0200

    fixed the problems

--- utility.js
+++ utility.js
@@ -6,33 +6,32 @@ utility.test = function() {
 
 utility.getUpgradersAmount = function() {
   var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-
 }
 
-
-
-function getBuildersAmount() {
+utility.getBuildersAmount = function() {
   var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 }
 
-function getHarvestersAmount() {
+utility.getHarvestersAmount = function() {
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
 }
 
-function spawnHarverster() {
+
+utility.spawnHarverster = function() {
   var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
     role: 'harvester'
   });
 }
 
-function spawnBuilder() {
+
+utility.spawnBuilder = function() {
   var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE],
     undefined, {
       role: 'builder'
     });
 }
 
-function spawnUpgrader() {
+utility.spawnUpgrader = function() {
   var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
     role: 'upgraders'
   });
@@ -40,7 +39,7 @@ function spawnUpgrader() {
 
 //Memory cleaning
 // gestorbene Creeps haben noch einen Memory dieser muss gelöscht werden
-function memoryCleaning() {
+utility.memoryCleaning = function() {
   for (var name in Memory.creeps) {
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
@@ -49,7 +48,5 @@ function memoryCleaning() {
   }
 
 }
-
-
 
 module.exports = utility;
