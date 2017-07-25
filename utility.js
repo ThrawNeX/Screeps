f1a94278fb1acb029338cf4e5487125f054a3d1e
mmezger <mmezger@stud.hs-heilbronn.de>
2017-07-25 17:23:12 +0200

    fix

--- utility.js
+++ utility.js
@@ -1,52 +1,52 @@
 var utility = {
 
-    function test() {
-      console.log("TESSSST");
-    }
+  function test() {
+    console.log("TESSSST");
+  }
 
-    function getUpgradersAmount() {
-      var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    }
+  function getUpgradersAmount() {
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  }
 
-    function getBuildersAmount() {
-      var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    }
+  function getBuildersAmount() {
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  }
 
-    function getHarvestersAmount() {
-      var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    }
+  function getHarvestersAmount() {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  }
 
-    function spawnHarverster() {
-      var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
-        role: 'harvester'
+  function spawnHarverster() {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
+      role: 'harvester'
+    });
+  }
+
+  function spawnBuilder() {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE],
+      undefined, {
+        role: 'builder'
       });
-    }
+  }
 
-    function spawnBuilder() {
-      var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE],
-        undefined, {
-          role: 'builder'
-        });
-    }
+  function spawnUpgrader() {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
+      role: 'upgraders'
+    });
+  }
 
-    function spawnUpgrader() {
-      var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {
-        role: 'upgraders'
-      });
-    }
-
-    //Memory cleaning
-    // gestorbene Creeps haben noch einen Memory dieser muss gelöscht werden
-    function memoryCleaning() {
-      for (var name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-          delete Memory.creeps[name];
-          console.log('Clearing non-existing creep memory:', name);
-        }
+  //Memory cleaning
+  // gestorbene Creeps haben noch einen Memory dieser muss gelöscht werden
+  function memoryCleaning() {
+    for (var name in Memory.creeps) {
+      if (!Game.creeps[name]) {
+        delete Memory.creeps[name];
+        console.log('Clearing non-existing creep memory:', name);
       }
-
     }
 
+  }
 
 
-    mode.exports = utility;
+}
+mode.exports = utility;
