2e3de19f7afba143e17a132ebfe38352380e1658
mmezger <mmezger@stud.hs-heilbronn.de>
2017-07-31 21:03:53 +0200

    asdf

--- role.builder.js
+++ role.builder.js
@@ -1,7 +1,7 @@
 //TODO: add if nothing to do fallback to upgrade or harvester
 
 var roleBuilder = {
-  var i = 0;
+
   /** @param {Creep} creep **/
   run: function(creep) {
 
@@ -30,14 +30,14 @@ var roleBuilder = {
       }
     } else {
       var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[i]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[i], {
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {
           visualizePathStyle: {
             stroke: '#ffaa00'
           }
         });
       }
-      if (creep.harvest(sources[i] == ERR_NO_PATH)) {
+      if (creep.harvest(sources[0] == ERR_NO_PATH)) {
         console.log("asdf");
 
       }
